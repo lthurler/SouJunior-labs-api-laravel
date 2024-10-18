1337ef5c53f7473d4fc889e6bb6e642ba4e50006
lthurler <leothurler@gmail.com>
2024-10-18 12:59:23 -0300

    PermissionUserController

--- app/Http/Controllers/User/PermissionUserController.php
+++ app/Http/Controllers/User/PermissionUserController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PermissionUserController extends Controller
 {
-    public function __invoke(string $uuid)
+    public function __invoke(Request $request, string $uuid)
     {
+        $user = User::query()->where('uuid', $uuid)->first();
+
+        if (is_null($user)) {
+            return response()->json([
+                'error' => 'Usuário não encontrado.',
+            ], 404);
+        }
+
+        if ($user->permission !== 'admin') {
+            return response()->json(
+                [
+                    'error' => 'Você não tem permissão para autalizar este produto.',
+                ],
+                403
+            );
+        }
+
+        $user->permission = $request->input('permission');
+        $user->save();
+
+        return response()->json(
+            [
+                'message' => 'Usuário atualizado com sucesso!',
+                'User ' => $user
+            ],
+            200
+        );
+
     }
 }
